50cea264036b55b593421482f25d82f14e68e0b4
JANG-MINWOO <alsdn2606@gmail.com>
2024-09-19 17:24:20 +0900

    feat: "Lv 2 기능 완료"
    출력개선 완료. 게임 시작시 1~3번으로 게임시작, 기록보기, 종료하기 추가하여 게임시작과 종료하기 기능 완성.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import BaseballGame.GameSetting.Game;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Game game = new Game();
-        game.start();
+
+        System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
+        System.out.println("1. 게임 시작하기 2. 게임 기록보기 3. 종료하기");
+        int userSelection = sc.nextInt();
+        if(userSelection == 1) {
+            System.out.println("<게임을 시작합니다>");
+            game.start();
+        }else if(userSelection == 3) {
+            System.out.println("종료합니다");
+        }
     }
 }
